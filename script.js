2209ed8f4d13d125ee40d85d2d69412daea97d89
VytasKitm <vytautas.cerniauskas@stud.kitm.lt>
2025-04-01 14:15:01 +0300

    creating new work

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log('hello')
+
+const input = document.createElement('input')
+const submit = document.createElement('button')
+
+input.setAttribute('class', 'input')
+submit.setAttribute('class', 'button')
+
+const container = document.getElementById('container')
+
+container.append(input, submit)
+
+
+
+
+
